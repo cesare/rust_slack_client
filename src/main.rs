44140c3a6cf076c58023dea153dec02c8ac8eef7
SAWADA Tadashi <cesare@mayverse.jp>
2018-09-02 21:30:57 +0900

    GET slack.com top page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate futures;
+extern crate hyper;
+extern crate hyper_tls;
+extern crate tokio;
+
+use futures::{future, Future};
+use hyper::Client;
+use hyper_tls::HttpsConnector;
+
 fn main() {
-    println!("Hello, world!");
+    tokio::run(future::lazy(|| {
+        let https = HttpsConnector::new(4).expect("TLS initialization failed");
+        let client = Client::builder()
+            .build::<_, hyper::Body>(https);
+
+        client.get("https://slack.com".parse().unwrap())
+            .map(|response| println!("{:?}", response))
+            .map_err(|error| println!("{:?}", error))
+    }));
 }
