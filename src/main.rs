d789ce85377a0e6810933bd4fa1521f4ee9b95e8
SAWADA Tadashi <cesare@mayverse.jp>
2018-09-03 22:35:14 +0900

    Enable authentication

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,28 @@ extern crate futures;
 extern crate hyper;
 extern crate hyper_tls;
 extern crate tokio;
+extern crate url;
+
+use std::env;
 
 use futures::{future, Future};
-use hyper::Client;
+use hyper::{Client, Uri};
 use hyper_tls::HttpsConnector;
+use url::form_urlencoded::Serializer;
+
+fn create_query_string(token: &String) -> String {
+    Serializer::new(String::new()).append_pair("token", token).finish()
+}
+
+fn create_authentication_uri(token: String) -> Uri {
+    let query = create_query_string(&token);
+    let url_string = format!("https://slack.com/api/rtm.connect?{}", query);
+    url_string.parse::<Uri>().unwrap()
+}
+
+fn find_token() -> String {
+    env::var("SLACK_TOKEN").unwrap_or_else(|_error| panic!("SLACK_TOKEN missing")).clone()
+}
 
 fn main() {
     tokio::run(future::lazy(|| {
@@ -13,7 +31,9 @@ fn main() {
         let client = Client::builder()
             .build::<_, hyper::Body>(https);
 
-        client.get("https://slack.com".parse().unwrap())
+        let token = find_token();
+        let uri = create_authentication_uri(token);
+        client.get(uri)
             .map(|response| println!("{:?}", response))
             .map_err(|error| println!("{:?}", error))
     }));
