323c3ea3b3ad7e3df3aca61cd9366f82af33d4d9
SAWADA Tadashi <cesare@mayverse.jp>
2021-02-14 21:21:54 +0900

    Minimize project

--- src/bin/show_identity.rs
+++ src/bin/show_identity.rs
@@ -1,29 +1,4 @@
-extern crate futures;
-extern crate http;
-extern crate hyper;
-extern crate hyper_tls;
-extern crate serde_json;
-extern crate tokio;
-extern crate url;
-
-extern crate slack_client;
-
-use futures::future;
-
-use slack_client::authentication::*;
-use slack_client::client::*;
-use slack_client::error::Error;
-
-
-fn start() -> Result<(), Error> {
-    let client = SlackApiClient::create()?;
-    let response = authenticate(&client)?;
-    println!("{:?}", response.body);
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
-}
-
-fn main() {
-    tokio::run(future::lazy(|| {
-        start().map_err(|error| println!("{:?}", error))
-    }));
 }
