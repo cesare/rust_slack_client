235e5b9b591988362144687d6401c7d8dc3bc156
SAWADA Tadashi <cesare@mayverse.jp>
2021-02-14 21:54:04 +0900

    Enable to call auth.test API

--- src/bin/show_identity.rs
+++ src/bin/show_identity.rs
@@ -1,4 +1,20 @@
+use hyper_tls::HttpsConnector;
+use hyper::{Body, Client, Request};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let slack_token = std::env::var("SLACK_TOKEN")?;
+
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+
+    let request = Request::builder()
+        .method("POST")
+        .uri("https://slack.com/api/auth.test")
+        .header("Authorization", format!("Bearer {}", slack_token))
+        .body(Body::empty())?;
+
+    let response = client.request(request).await?;
+    println!("{:?}", response);
     Ok(())
 }
