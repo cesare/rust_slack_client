e905d3815a0841556aeab0d36e61264de7fb0d32
SAWADA Tadashi <cesare@mayverse.jp>
2021-02-15 21:40:42 +0900

    Enable to list conversations in raw JSON

--- src/bin/list_conversations.rs
+++ src/bin/list_conversations.rs
@@ -1,37 +1,34 @@
-extern crate futures;
-extern crate http;
-extern crate hyper;
-extern crate hyper_tls;
-extern crate serde_json;
-extern crate tokio;
-extern crate url;
+use hyper_tls::HttpsConnector;
+use hyper::{Body, Client, Request};
+use hyper::client::HttpConnector;
+use serde_json::Value;
 
-extern crate slack_client;
-
-use futures::future;
-
-use slack_client::client::*;
-use slack_client::conversation::*;
-use slack_client::error::Error;
-
-
-fn show_channel(ch: &Channel) {
-    println!("{} {}", ch.id, ch.name);
+fn create_client() -> Client<HttpsConnector<HttpConnector>, Body> {
+    let https = HttpsConnector::new();
+    Client::builder().build::<_, hyper::Body>(https)
 }
 
-fn start() -> Result<(), Error> {
-    let client = SlackApiClient::create()?;
-    let request = ListChannelsRequest::new();
-    let response: ListChannelsResponse = client.get(&request)?;
+fn create_request(slack_token: &str) -> Result<Request<Body>, hyper::http::Error> {
+    Request::builder()
+        .method("GET")
+        .uri("https://slack.com/api/conversations.list")
+        .header("Authorization", format!("Bearer {}", slack_token))
+        .body(Body::empty())
+}
 
-    for ch in &response.body.channels {
-        show_channel(ch);
-    }
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let slack_token = std::env::var("SLACK_TOKEN")?;
+
+    let client = create_client();
+    let request = create_request(&slack_token)?;
+
+    let mut response = client.request(request).await?;
+    let body = response.body_mut();
+    let bytes: hyper::body::Bytes = hyper::body::to_bytes(body).await?;
+
+    let json: Value = serde_json::from_slice(bytes.as_ref())?;
+    println!("{:?}", json);
+
     Ok(())
-}
-
-fn main() {
-    tokio::run(future::lazy(|| {
-        start().map_err(|error| println!("{:?}", error))
-    }));
 }
