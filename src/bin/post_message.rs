8e0727123ee175fb6c29458b0c615e030b6fbf5b
SAWADA Tadashi <cesare@mayverse.jp>
2021-02-16 02:21:03 +0900

    Enable to post message

--- src/bin/post_message.rs
+++ src/bin/post_message.rs
@@ -1,35 +1,46 @@
-extern crate futures;
-extern crate http;
-extern crate hyper;
-extern crate hyper_tls;
-extern crate serde_json;
-extern crate tokio;
-extern crate url;
-
-extern crate slack_client;
-
-use futures::future;
+use hyper_tls::HttpsConnector;
+use hyper::{Body, Client, Request};
+use hyper::client::HttpConnector;
+use serde_json::Value;
 
 use std::env;
 
-use slack_client::client::*;
-use slack_client::error::Error;
-use slack_client::message::*;
-
-fn start(channel: String, text: String) -> Result<(), Error> {
-    let client = SlackApiClient::create()?;
-    let request = PostMessageRequest::new(channel, text);
-    let response: PostMessageResponse = client.post(&request)?;
-    println!("{:?}", response.body);
-    Ok(())
+fn create_client() -> Client<HttpsConnector<HttpConnector>, Body> {
+    let https = HttpsConnector::new();
+    Client::builder().build::<_, hyper::Body>(https)
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let channel = args[1].to_string();
-    let text = args[2].to_string();
+fn create_request(slack_token: &str, channel: &str, text: &str) -> Result<Request<Body>, hyper::http::Error> {
+    let query = form_urlencoded::Serializer::new(String::new())
+        .append_pair("channel", channel)
+        .append_pair("text", text)
+        .finish();
 
-    tokio::run(future::lazy(|| {
-        start(channel, text).map_err(|error| println!("{:?}", error))
-    }));
+    Request::builder()
+        .method("POST")
+        .uri("https://slack.com/api/chat.postMessage")
+        .header("Authorization", format!("Bearer {}", slack_token))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(query.into())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    let channel = &args[1];
+    let text = &args[2];
+
+    let slack_token = std::env::var("SLACK_TOKEN")?;
+
+    let client = create_client();
+    let request = create_request(&slack_token, channel, text)?;
+
+    let mut response = client.request(request).await?;
+    let body = response.body_mut();
+    let bytes: hyper::body::Bytes = hyper::body::to_bytes(body).await?;
+
+    let json: Value = serde_json::from_slice(bytes.as_ref())?;
+    println!("{:?}", json);
+
+    Ok(())
 }
