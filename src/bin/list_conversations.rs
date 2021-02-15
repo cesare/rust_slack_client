a997773d7640ef3f1aa6c3b38f089aed96c464ff
SAWADA Tadashi <cesare@mayverse.jp>
2021-02-15 21:54:03 +0900

    Enable to show conversations with structs

--- src/bin/list_conversations.rs
+++ src/bin/list_conversations.rs
@@ -1,7 +1,37 @@
 use hyper_tls::HttpsConnector;
 use hyper::{Body, Client, Request};
 use hyper::client::HttpConnector;
+use serde::Deserialize;
 use serde_json::Value;
+
+#[derive(Debug, Deserialize)]
+struct Topic {
+    value: String,
+    creator: String,
+    last_set: u32,
+}
+
+#[derive(Debug, Deserialize)]
+struct Purpose {
+    value: String,
+    creator: String,
+    last_set: u32,
+}
+
+#[derive(Debug, Deserialize)]
+struct Channel {
+    id: String,
+    name: String,
+    num_members: u32,
+    is_private: bool,
+    topic: Topic,
+    purpose: Purpose,
+}
+
+#[derive(Debug, Deserialize)]
+struct Channels {
+    channels: Vec<Channel>,
+}
 
 fn create_client() -> Client<HttpsConnector<HttpConnector>, Body> {
     let https = HttpsConnector::new();
@@ -28,7 +58,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let bytes: hyper::body::Bytes = hyper::body::to_bytes(body).await?;
 
     let json: Value = serde_json::from_slice(bytes.as_ref())?;
-    println!("{:?}", json);
+    let channels: Channels = serde_json::from_value(json)?;
+    println!("{:?}", channels);
 
     Ok(())
 }
