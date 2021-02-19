a6008a9ddcc39170db5844c4251377cac360861c
SAWADA Tadashi <cesare@mayverse.jp>
2021-02-19 23:13:42 +0900

    Enable to parse messages

--- src/events.rs
+++ src/events.rs
@@ -6,6 +6,16 @@ pub enum Message {
     #[serde(rename = "hello")]
     Hello {},
 
+    #[serde(rename = "message")]
+    Message {
+        team: String,
+        channel: String,
+        user: String,
+        user_team: String,
+        text: String,
+        ts: String,
+    },
+
     #[serde(rename = "user_typing")]
     UserTyping {
         channel: String,
