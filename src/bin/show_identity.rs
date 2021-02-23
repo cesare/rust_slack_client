e55c9abcfb2a0932424168c1c2cd35d7d3256d0c
SAWADA Tadashi <cesare@mayverse.jp>
2021-02-23 20:15:33 +0900

    Refactor auth.test request into a SlackApiRequest

--- src/bin/show_identity.rs
+++ src/bin/show_identity.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
-use hyper::{Body, Request};
 use serde::Deserialize;
 
-use slack_client::client;
+use slack_client::client::SlackApiClient;
+use slack_client::requests::AuthTestRequest;
 
 #[derive(Debug, Deserialize)]
 struct AuthTest {
@@ -16,23 +16,12 @@ struct AuthTest {
     is_enterprise_install: bool,
 }
 
-fn create_request(slack_token: &str) -> Result<Request<Body>> {
-    let request = Request::builder()
-        .method("POST")
-        .uri("https://slack.com/api/auth.test")
-        .header("Authorization", format!("Bearer {}", slack_token))
-        .body(Body::empty())?;
-    Ok(request)
-}
-
 #[tokio::main]
 async fn main() -> Result<()> {
-    let slack_token = std::env::var("SLACK_TOKEN")?;
+    let client = SlackApiClient::new();
+    let request = AuthTestRequest::new();
 
-    let client = client::create_client();
-    let request = create_request(&slack_token)?;
-
-    let mut response = client.request(request).await?;
+    let mut response = client.request(&request).await?;
     let body = response.body_mut();
     let bytes: hyper::body::Bytes = hyper::body::to_bytes(body).await?;
     let auth_test: AuthTest = serde_json::from_slice(bytes.as_ref())?;
--- src/requests.rs
+++ src/requests.rs
@@ -4,3 +4,25 @@ use hyper::{Body, Request};
 pub trait SlackApiRequest {
     fn build(self: &Self) -> Result<Request<Body>>;
 }
+
+pub struct AuthTestRequest {
+}
+
+impl AuthTestRequest {
+    pub fn new() -> Self {
+        AuthTestRequest {}
+    }
+}
+
+impl SlackApiRequest for AuthTestRequest {
+    fn build(self: &Self) -> Result<Request<Body>> {
+        let slack_token = std::env::var("SLACK_TOKEN")?;
+
+        let request = Request::builder()
+            .method("POST")
+            .uri("https://slack.com/api/auth.test")
+            .header("Authorization", format!("Bearer {}", slack_token))
+            .body(Body::empty())?;
+        Ok(request)
+    }
+}
