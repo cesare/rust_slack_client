b246ab0ffce1af7df7fef1cafb5ca47571367f42
SAWADA Tadashi <cesare@mayverse.jp>
2021-02-26 23:31:26 +0900

    Refactor wait_for_messages

--- src/bin/bot.rs
+++ src/bin/bot.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 use futures::stream::{Stream, StreamExt};
-use serde_json::Value;
 use tokio_tungstenite::tungstenite::Error as WsError;
 use tokio_tungstenite::tungstenite::Message;
 
@@ -9,17 +8,14 @@ use slack_client::events;
 use slack_client::requests::RtmConnectRequest;
 use slack_client::responses::RtmConnect;
 
-async fn wait_for_messages(stream: &mut (dyn Stream<Item = Result<Message, WsError>> + Unpin)) -> Result<()> {
+async fn wait_for_messages(stream: &mut (dyn Stream<Item = Result<Message, WsError>> + Unpin)) {
     while let Some(Ok(message)) = stream.next().await {
         if let Message::Text(text) = message {
-            let json: Value = serde_json::from_str(&text)?;
-            if let Ok(msg) = serde_json::from_value::<events::Message>(json) {
+            if let Ok(msg) = serde_json::from_str::<events::Message>(&text) {
                 println!("{:?}", msg);
             }
         }
     }
-
-    Ok(())
 }
 
 #[tokio:: main]
@@ -31,7 +27,7 @@ async fn main() -> Result<()> {
     println!("{:?}", rtm_connect);
 
     let (mut stream, _response) = tokio_tungstenite::connect_async(rtm_connect.url).await?;
-    wait_for_messages(&mut stream).await?;
+    wait_for_messages(&mut stream).await;
 
     Ok(())
 }
