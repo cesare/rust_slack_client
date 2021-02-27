7b8f62a65f33ec9e4411d91ac51f87a3093879be
SAWADA Tadashi <cesare@mayverse.jp>
2021-02-27 13:40:20 +0900

    Set alias for tungstenite message

--- src/bin/bot.rs
+++ src/bin/bot.rs
@@ -2,16 +2,16 @@ use anyhow::Result;
 use futures::stream::{Stream, StreamExt};
 use tokio::sync::mpsc::{channel, Sender};
 use tokio_tungstenite::tungstenite::Error as WsError;
-use tokio_tungstenite::tungstenite::Message;
+use tokio_tungstenite::tungstenite::Message as WsMessage;
 
 use slack_client::client::SlackApiClient;
 use slack_client::events;
 use slack_client::requests::RtmConnectRequest;
 use slack_client::responses::RtmConnect;
 
-async fn wait_for_messages(stream: &mut (dyn Stream<Item = Result<Message, WsError>> + Unpin + Send), tx: &mut Sender<events::Message>) {
+async fn wait_for_messages(stream: &mut (dyn Stream<Item = Result<WsMessage, WsError>> + Unpin + Send), tx: &mut Sender<events::Message>) {
     while let Some(Ok(message)) = stream.next().await {
-        if let Message::Text(text) = message {
+        if let WsMessage::Text(text) = message {
             if let Ok(msg) = serde_json::from_str::<events::Message>(&text) {
                 let _ = tx.send(msg).await;
             }
