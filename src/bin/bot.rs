6f252adddccbe45dc1a1fe66bfa635260df51789
SAWADA Tadashi <cesare@mayverse.jp>
2021-03-02 09:23:43 +0900

    Add struct MessageHandler

--- src/bin/bot.rs
+++ src/bin/bot.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use futures::stream::{Stream, StreamExt};
-use tokio::sync::mpsc::{channel, Sender};
+use tokio::sync::Mutex;
+use tokio::sync::mpsc::{channel, Receiver, Sender};
 use tokio_tungstenite::tungstenite::Error as WsError;
 use tokio_tungstenite::tungstenite::Message as WsMessage;
 
@@ -9,19 +10,39 @@ use slack_client::events::Message;
 use slack_client::requests::{PostMessageRequest, RtmConnectRequest};
 use slack_client::responses::RtmConnect;
 
-async fn handle_message(msg: &Message) -> Result<()> {
-    match msg {
-        Message::Message { channel, user, text, ..} => {
-            if text == "ping" {
-                let client = SlackApiClient::new();
-                let reply = format!("<@{}> pong", user);
-                let request = PostMessageRequest::new(channel, &reply);
-                client.request(&request).await?;
-            }
+struct MessageHandler {
+    rx: Mutex<Receiver<Message>>,
+}
+
+impl MessageHandler {
+    fn new(rx: Receiver<Message>) -> Self {
+        MessageHandler {
+            rx: Mutex::new(rx),
         }
-        _ => {}
     }
-    Ok(())
+
+    async fn run(&self) {
+        let mut rx = self.rx.lock().await;
+        while let Some(msg) = rx.recv().await {
+            println!("{:?}", msg);
+            let _ = self.handle_message(&msg).await;
+        }
+    }
+
+    async fn handle_message(&self, msg: &Message) -> Result<()> {
+        match msg {
+            Message::Message { channel, user, text, ..} => {
+                if text == "ping" {
+                    let client = SlackApiClient::new();
+                    let reply = format!("<@{}> pong", user);
+                    let request = PostMessageRequest::new(channel, &reply);
+                    client.request(&request).await?;
+                }
+            }
+            _ => {}
+        }
+        Ok(())
+    }
 }
 
 async fn wait_for_messages<S>(stream: &mut S, tx: &mut Sender<Message>)
@@ -44,12 +65,9 @@ async fn main() -> Result<()> {
     let rtm_connect: RtmConnect = client.request(&request).await?;
     println!("{:?}", rtm_connect);
 
-    let (mut tx, mut rx) = channel::<Message>(100);
+    let (mut tx, rx) = channel::<Message>(100);
     let msg_handle = tokio::spawn(async move {
-        while let Some(msg) = rx.recv().await {
-            println!("{:?}", msg);
-            let _ = handle_message(&msg).await;
-        }
+        MessageHandler::new(rx).run().await;
     });
 
     let (mut stream, _response) = tokio_tungstenite::connect_async(rtm_connect.url).await?;
